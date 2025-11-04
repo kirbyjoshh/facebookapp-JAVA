ee22b0ece0ab0ddabb2f5c2bd079d2543c444eb9
kirbyjoshh <kjmbondoc.student@ua.edu.ph>
2025-11-04 15:27:48 +0800

    updated postcontroller

--- src/main/java/com/bondoc/facebookapp/PostController.java
+++ src/main/java/com/bondoc/facebookapp/PostController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class PostController {
 
     private final PostRepository postRepository;
